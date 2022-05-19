d9fc54e04ebd0dd6b836853cd310c51e93bbce33
SwartzJonah <swartzjonah@gmail.com>
2022-05-18 21:18:17 -0400

    added color and buttons and some style

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 const gridContainer = document.querySelector('#gridContainer');
 const resizeBtn = document.querySelector('#resizeBtn');
+const colorswapBtn = document.querySelector('#colorswapBtn');
+const blackswapBtn = document.querySelector('#blackswapBtn');
+const incrementBtn = document.querySelector('#incrementBtn');
+const startnewBtn = document.querySelector('#startnewBtn');
+const eraseBtn = document.querySelector('#eraseBtn');
 let size = 16;
+let colorState = 'black';
 
 function createGrid(size){    
     for (let i = 1; i < (size * size) + 1; i++){
@@ -9,7 +15,15 @@ function createGrid(size){
         //gridPiece.textContent = `${i}`;
         gridContainer.appendChild(gridPiece);
         gridPiece.addEventListener('mouseover', function (e) {
-            e.target.style.background = 'black';  
+            if (colorState == 'black'){
+                e.target.style.background = 'black'; 
+            } else if (colorState == 'color'){
+                e.target.style.background = colorChooser();
+            } else if (colorState == 'white'){
+                e.target.style.background = 'white';
+            } else {
+                e.target.style.background = 'black';
+            }
         });
     }
 }
@@ -22,7 +36,17 @@ function deleteGrid(){
         )
 
 }
+function randomNumber(max){
+   return Math.floor(Math.random() * max);
+}
 
+function colorChooser(){
+    let r = randomNumber(255);
+    let g = randomNumber(255);
+    let b = randomNumber(255);
+    let rgb = `rgb(${r},${g},${b})`;
+    return rgb;    
+}
 
 createGrid(16);
 
@@ -39,3 +63,26 @@ resizeBtn.addEventListener('click', () => {
     document.querySelector('#gridContainer').style.gridTemplateColumns = `repeat(${size}, auto)`;
     }
 });
+
+colorswapBtn.addEventListener('click', () => {
+    colorState = 'color';
+})
+
+blackswapBtn.addEventListener('click', () => {
+    colorState = 'black';
+})
+
+eraseBtn.addEventListener('click', () => {
+    colorState = 'white';
+})
+
+incrementBtn.addEventListener('click', () => {
+    colorState = 'increment';
+})
+
+startnewBtn.addEventListener('click', () => {
+    deleteGrid();
+    createGrid(size);
+})
+        
+      
