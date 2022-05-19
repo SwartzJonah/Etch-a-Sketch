c4a6126911bf280c5987b0ae2af553fee216418b
SwartzJonah <swartzjonah@gmail.com>
2022-05-19 00:13:29 -0400

    Incremental now works for white and colors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Initializing Starter State
 const gridContainer = document.querySelector('#gridContainer');
 const resizeBtn = document.querySelector('#resizeBtn');
 const colorswapBtn = document.querySelector('#colorswapBtn');
@@ -5,28 +6,35 @@ const blackswapBtn = document.querySelector('#blackswapBtn');
 const incrementBtn = document.querySelector('#incrementBtn');
 const startnewBtn = document.querySelector('#startnewBtn');
 const eraseBtn = document.querySelector('#eraseBtn');
+const removeLinesBtn = document.querySelector('#removeLinesBtn');
 let size = 16;
 let colorState = 'black';
+createGrid(16);
 
+//functions
+
+//Makes grid and sets style based 
 function createGrid(size){    
     for (let i = 1; i < (size * size) + 1; i++){
         let gridPiece = document.createElement('div');
         gridPiece.classList.add('gridPiece');
         //gridPiece.textContent = `${i}`;
         gridContainer.appendChild(gridPiece);
+        gridPiece.style.backgroundColor = 'rgba(0,0,0, 0.01)';
         gridPiece.addEventListener('mouseover', function (e) {
             if (colorState == 'black'){
-                e.target.style.background = 'black'; 
+                e.target.style.backgroundColor = `rgba(0,0,0,0.9)`; 
             } else if (colorState == 'color'){
-                e.target.style.background = colorChooser();
+                e.target.style.backgroundColor = colorChooser();
             } else if (colorState == 'white'){
-                e.target.style.background = 'white';
+                e.target.style.backgroundColor = 'rgba(0,0,0, 0.01)';
             } else {
-                e.target.style.background = 'black';
+                e.target.style.backgroundColor = darken(e.target.style.backgroundColor);
             }
         });
     }
 }
+
 
 function deleteGrid(){
     const grids = gridContainer.querySelectorAll(`div`);
@@ -37,19 +45,39 @@ function deleteGrid(){
 
 }
 function randomNumber(max){
-   return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
+}
+
+function randomOpacity(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function colorChooser(){
     let r = randomNumber(255);
     let g = randomNumber(255);
     let b = randomNumber(255);
-    let rgb = `rgb(${r},${g},${b})`;
-    return rgb;    
+    let a = randomOpacity(5,9)/10;
+    let rgba = `rgba(${r},${g},${b},${a})`;
+    console.log(rgba);
+    return rgba;    
 }
 
-createGrid(16);
 
+function darken(color){
+    let amountGray = (color.slice(-4, -1));
+    let newColor =  color.slice(0, -4);
+    if (amountGray < 0.9) {
+        amountGray = Number(amountGray) + .1;
+        amountGray = amountGray.toFixed(1);
+        color = `${newColor}${amountGray})`;
+        return color;
+    } else {
+        return color;
+    }
+    
+}
+
+//Button Listeners
 resizeBtn.addEventListener('click', () => {
     size = prompt("Please give a new size");
     size =  parseInt(size);
@@ -83,6 +111,11 @@ incrementBtn.addEventListener('click', () => {
 startnewBtn.addEventListener('click', () => {
     deleteGrid();
     createGrid(size);
+})
+
+removeLinesBtn.addEventListener('click', () => {
+   
+      
 })
         
       
